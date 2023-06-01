bded13f90e1db3a24ab6fd6965519ba1556cb7c4
AvokadoPo4ka <88880953+AvokadoPo4ka@users.noreply.github.com>
2023-06-01 15:15:23 +0700

    Add files via upload

---
+++ UnityMethodsGit/UnityMethods/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using mm = UnityMethods.MonobehaivourMethods;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace UnityMethods
+{
+    public static class Methods
+    {
+        public static void DebugInConsole(string msg)
+        {
+            mm.print(msg);
+        }
+        public static void CreateRandom(int minVal, int maxVal)
+        {
+            Random.Range(minVal, maxVal);
+        }
+        public static void CreateRandomFloat(float minVal, float maxVal)
+        {
+            Random.Range(minVal, maxVal);
+        }
+        public static void ChangeText(string text, Text TextToChange) => TextToChange.text = text;
+        public static IEnumerator WaitForSeconds(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            yield return null;
+        }
+    }
+}
---
+++ UnityMethodsGit/UnityMethods/MonobehaivourMethods.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UnityMethods
+{
+    public class MonobehaivourMethods : MonoBehaviour
+    {
+        public void Write(string message) => print(message);
+    }
+}
